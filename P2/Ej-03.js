eafffabda3bbd85d489186473431ee643336dcdd
narroyoc96 <n.arroyoc.2017@alumnos.urjc.es>
2021-03-16 23:31:22 +0100

    cambiados de sisitio

--- P2/Ej-03.js
+++
@@ -1,8 +0,0 @@
-// Esto son comentarios en js
-// Mi primer programa en JS!!!
-
-//-- Imprimir tres mensajes en la consola
-console.log("Estoy fuera del HTML");
-console.log("Se recomienda hacerlo así");
-console.log("------Noelia Arroyo-------")
-
