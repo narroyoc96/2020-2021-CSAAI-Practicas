7a098ccb0e666ad87f4c1c6d8ebf1f7ad4ae441d
narroyoc96 <n.arroyoc.2017@alumnos.urjc.es>
2021-03-29 17:25:56 +0200

    fichero js funciona con calculadora

---
+++ P2/index.js
@@ -0,0 +1,39 @@
+console.log("Ejecutando JS...");
+
+display = document.getElementById("display")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+numeros = document.getElementsByClassName('numero')
+calculos = document.getElementsByClassName('operacion')
+
+for(i=0; i<numeros.length; i++){
+  numeros[i].onclick = (ev) =>{
+    digito(ev.target);
+  }
+}
+for(i=0; i<calculos.length; i++){
+  calculos[i].onclick = (ev) =>{
+    display.innerHTML += ev.target.value;
+  }
+}
+
+function digito(boton)
+{
+  if (display.innerHTML == "0"){
+    display.innerHTML = boton.value;
+  } else{
+    display.innerHTML += boton.value;
+  }
+}
+
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  display.innerHTML = eval(display.innerHTML);
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display.innerHTML = "0";
+}
