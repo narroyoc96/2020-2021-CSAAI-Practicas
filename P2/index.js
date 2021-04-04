92da5e1b6769dbd0535541c1d89f245d60f28760
narroyoc96 <n.arroyoc.2017@alumnos.urjc.es>
2021-04-04 21:21:50 +0200

    sin estados

--- P2/index.js
+++ P2/index.js
@@ -9,20 +9,9 @@ porcentaje = document.getElementById('porcentaje')
 raiz = document.getElementById('raiz')
 ans = document.getElementById('ans')
 
-/*Estados Calculadora*/
-const ESTADO = {
-  INIT: 0,
-  OP1: 1,
-  OPERATION: 2,
-  OP2: 3,
-  RESULT: 4,
-}
+numeros = document.getElementsByClassName('numero')
+calculos = document.getElementsByClassName('operacion')
 
-let estado = ESTADO.INIT;
-
-/*Array con los elementos de la clase numero y la clase operacion*/
-let numeros = document.getElementsByClassName('numero')
-let calculos = document.getElementsByClassName('operacion')
 
 for(i=0; i<numeros.length; i++){
   numeros[i].onclick = (ev) =>{
@@ -36,63 +25,33 @@ for(i=0; i<calculos.length; i++){
   }
 }
 
-function digito(boton) {
-  if (estado == ESTADO.INIT){
-    display.innerHTML = boton;
-    estado = ESTADO.OP1;
-    console.log(estado,"Operador 1")
-  } else if (estado == ESTADO.OP1 || estado == ESTADO.OP2 || estado == ESTADO.OPERATION){
-    display.innerHTML += boton;
-    if (estado == ESTADO.OPERATION) {
-      estado = ESTADO.OP2;
-      console.log(estado,"Operador 2");
-    }
+function digito(boton)
+{
+  if (display.innerHTML == "0"){
+    display.innerHTML = boton.value;
+  } else{
+    display.innerHTML += boton.value;
   }
-}
-
-for (let operando of operacion){
-  operando.onclick = (ev) => {
-      display.innerHTML += ev.target.value;
-      console.log("OPERADOR");
-  } 
 }
 
 //-- Evaluar la expresion
 igual.onclick = () => {
-  if(estado == ESTADO.OP1 || estado == ESTADO.OP2){
-    display.innerHTML = eval(display.innerHTML);
-    estado = ESTADO.INIT;
-    console.log(estado,"igual");
-  }
+  display.innerHTML = eval(display.innerHTML);
 }
 
 //-- Poner a cero la expresion
 clear.onclick = () => {
-  display.innerHTML = " ";
-  estado = ESTADO.OP1;
+  display.innerHTML = "0";
 }
 
 //-- Poner punto en la expresion
 punto.onclick = () =>{
-  if(estado != ESTADO.OP1 && estado != ESTADO.OP2){
-    console.log("ERROR");
-  }else{
-    display.innerHTML += ev.target.value;
-    console.log(estado,"Ejecucción correcta");
-  }
+    display.innerHTML += punto.value;
 }
 
 //-- Eliminar un digito en la expresion
 del.onclick = () => {
-  if (display.innerHTML.length == 1) {
-    display.innerHTML = "";
-    estado = ESTADO.INIT
-  } else {
-    display.innerHTML = display.innerHTML.substring(0, display.innerHTML.length - 1);
-    if (estado == ESTADO.OPERATION){
-      estado = ESTADO.OP1;
-    }
-  }
+    display.innerHTML = display.innerHTML.slice(0,-1);
 }
 
 //-- Calcular porcentaje en la expresion
