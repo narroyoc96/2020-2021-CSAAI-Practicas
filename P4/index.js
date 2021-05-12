d9055b066ffe39b46a8be8c74231878f5cac9cb6
narroyoc96 <n.arroyoc.2017@alumnos.urjc.es>
2021-05-12 19:57:56 +0200

    filtro gris, filtro color y deslizadores

--- P4/index.js
+++ P4/index.js
@@ -2,8 +2,25 @@ console.log("Ejecutando JS....")
 
 //-- Obtener elementos del DOM
 const canvas = document.getElementById('canvas');
-const img = document.getElementById('imagesrc')
+const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
+
+//Botones
+const grises = document.getElementById('gris');
+const colores = document.getElementById('color')
+
+//Valores deslizador
+const range_value_R = document.getElementById('range_value_R');
+const range_value_G = document.getElementById('range_value_G');
+const range_value_B = document.getElementById('range_value_B');
+const range_value_gray = document.getElementById('range_value_gray');
+
+//Acceso deslizador
+const deslizador_R = document.getElementById('deslizador_R');
+const deslizador_G = document.getElementById('deslizador_G');
+const deslizador_B = document.getElementById('deslizador_B');
+const deslizador_gray = document.getElementById('deslizador_gray');
+
 
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
@@ -20,24 +37,88 @@ img.onload = function () {
   //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img, 0,0);
 
-  //-- Obtener la imagen del canvas en pixeles
+  console.log("Imagen lista...");
+};
+
+
+function colors(){
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  ctx.drawImage(img, 0,0);
+
+  //Obtener la imagen del canvas en pixeles
   let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-  //-- Obtener el array con todos los píxeles
+  //Obtener el array con todos los píxeles
   let data = imgData.data
 
-  //-- Obtener el numero total de elementos en el array
-  console.log("Tamaño de data: " + data.length)
+  //Mostrar el nuevo valor de los deslizadores
+  range_value_R.innerHTML = deslizador_R.value;
+  range_value_G.innerHTML = deslizador_G.value;
+  range_value_B.innerHTML = deslizador_B.value;
 
-  //-- El número total de pixeles es la altura por la anchura
-  npixels = canvas.width * canvas.height
-  console.log("Anchura (en pixeles): " + canvas.width)
-  console.log("Altura (en pixeles): " + canvas.height)
-  console.log("Pixeles totales: " + npixels)
+  //Obtener el umbral de los colores de los deslizadores
+  var umbral_R = deslizador_R.value;
+  var umbral_G = deslizador_G.value;
+  var umbral_B = deslizador_B.value;
 
-  //-- Puesto que cada pixel ocupa 4 bytes, el array de píxeles
-  //-- tiene un tamaño de 4 * numero de pixeles
-  console.log("Total de datos de la imagen: " + npixels * 4)
-};
+  //Filtrar la imagen según el nuevo umbral
+  for (let i = 0; i < data.length; i+=4) {
+    if (data[i] > umbral_R)
+      data[i] = umbral_R;
+    if (data[i+1] > umbral_G)
+      data[i+1] = umbral_G;
+    if (data[i+2] > umbral_B)
+      data[i+2] = umbral_B;
+    }
+
+  //Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+}
+
+//Funcion de retrollamada de los deslizadores
+deslizador_R.oninput = () => {
+  colors();
+}
+deslizador_G.oninput = () => {
+  colors();
+}
+deslizador_B.oninput = () => {
+  colors();
+}
+
+
+//boton gris
+grises.onclick = () => {
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+  //-- Filtrar la imagen según el nuevo umbral
+  for (let i = 0; i < data.length; i+=4) {
+    var R = data[i];
+    var G = data[i+1];
+    var B = data[i+2];
+    var gris = (3 * R + 4 * G + B)/8;
+    gris = data[i] = data[i+1] = data[i+2];
+    }
+
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+}
+
+
+//boton colores
+colores.onclick = () => {
+    ctx.drawImage(img, 0,0);
+    deslizador_R.value = 255;
+    range_value_R.innerHTML = deslizador_R.value;
+    deslizador_G.value = 255;
+    range_value_G.innerHTML = deslizador_G.value;
+    deslizador_B.value = 255;
+    range_value_B.innerHTML = deslizador_B.value;
+
+}
 
 console.log("Fin...");
